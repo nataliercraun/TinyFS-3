e9497099397fb08dfcf5599ccdd58bbfc47e8ec8
Milan Goyal <Milan@Milans-MacBook-Pro.local>
2019-04-18 14:11:24 -0700

    RID done

--- src/com/client/RID.java
+++ src/com/client/RID.java
@@ -1,5 +1,11 @@
 package com.client;
 
 public class RID {
+	public String ChunkHandle;
+	public int index;
 	
+	public RID(String ChunkHandle, int index) {
+		this.ChunkHandle = ChunkHandle;
+		this.index = index;
+	}
 }
